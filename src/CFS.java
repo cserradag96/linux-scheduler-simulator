918ac4ba3f70f1107a84fccb330e1ab18210d65a
Jose Acevedo <13-10006@usb.ve>
2019-09-29 14:16:28 +0200

    Add RBTree Implementation and Tests

---
+++ src/CFS.java
@@ -0,0 +1,11 @@
+import lib.RedBlackTree;
+
+public class CFS {
+
+    public static void main(String [] args) {
+
+        // Test RedBlackTree
+        RedBlackTree bst = new RedBlackTree();
+        bst.test();
+    }
+}
